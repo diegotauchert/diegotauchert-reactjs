b9d5d8ae55aeec18de8d239e635993cd410579c5
Diego Tauchert <diego.tauchert@gmail.com>
2020-07-05 20:40:42 -0300

    Updates

---
+++ precache-manifest.eb9a7d67ec164b95f69b9f262a5b9c71.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3914ec95c9164ec8912dfafbd7c099b",
+    "url": "/diegotauchert-reactjs/index.html"
+  },
+  {
+    "revision": "848a8d7496c0632b0420",
+    "url": "/diegotauchert-reactjs/static/css/main.524cdfc2.chunk.css"
+  },
+  {
+    "revision": "ebda83bf6217b6d4d0e3",
+    "url": "/diegotauchert-reactjs/static/js/2.10855cdd.chunk.js"
+  },
+  {
+    "revision": "1f1578a1b4e3ebd16a162194ace2b21c",
+    "url": "/diegotauchert-reactjs/static/js/2.10855cdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "848a8d7496c0632b0420",
+    "url": "/diegotauchert-reactjs/static/js/main.b6738628.chunk.js"
+  },
+  {
+    "revision": "0d0371279d88013579d6",
+    "url": "/diegotauchert-reactjs/static/js/runtime-main.2433f222.js"
+  },
+  {
+    "revision": "1e8a605518fe5ab3c4f08eb9c7938e6f",
+    "url": "/diegotauchert-reactjs/static/media/bg-dialog.1e8a6055.png"
+  },
+  {
+    "revision": "cc83c3639e313d564dd0721b0c9bac6a",
+    "url": "/diegotauchert-reactjs/static/media/bola-2.cc83c363.svg"
+  },
+  {
+    "revision": "a57b5434c7a840b8e87c743e5e4acbfd",
+    "url": "/diegotauchert-reactjs/static/media/bola-3.a57b5434.svg"
+  },
+  {
+    "revision": "a565db1a1d81881c6a990f6f8eb12d05",
+    "url": "/diegotauchert-reactjs/static/media/btn-sound.a565db1a.svg"
+  },
+  {
+    "revision": "593780cdae949b6ad09f41aa410a2970",
+    "url": "/diegotauchert-reactjs/static/media/css3.593780cd.png"
+  },
+  {
+    "revision": "0748ef23f18f9c08ea558cdb218f9bc5",
+    "url": "/diegotauchert-reactjs/static/media/diego.0748ef23.png"
+  },
+  {
+    "revision": "149c441be1b396d8f9d9d35a02db9cc3",
+    "url": "/diegotauchert-reactjs/static/media/jaba-1.149c441b.jpg"
+  },
+  {
+    "revision": "3c118bcd8158ce2751cfb877fc41b55f",
+    "url": "/diegotauchert-reactjs/static/media/jaba-2.3c118bcd.jpg"
+  },
+  {
+    "revision": "d11bc65699c9416e398e42041ed9f023",
+    "url": "/diegotauchert-reactjs/static/media/jaba-3.d11bc656.jpg"
+  },
+  {
+    "revision": "ce0b2074d682892c4f3ca0499066a55f",
+    "url": "/diegotauchert-reactjs/static/media/laravel.ce0b2074.png"
+  },
+  {
+    "revision": "070dea14978151a27403276d6a51dfcf",
+    "url": "/diegotauchert-reactjs/static/media/linkedin.070dea14.png"
+  },
+  {
+    "revision": "375b43692f9717242066b72ca76e1f25",
+    "url": "/diegotauchert-reactjs/static/media/mongo.375b4369.png"
+  },
+  {
+    "revision": "010dd6b059d26fe32eadb7b3e31b2703",
+    "url": "/diegotauchert-reactjs/static/media/musician-elements.010dd6b0.png"
+  },
+  {
+    "revision": "d4dd0ae04a56235e90771c971b3b7267",
+    "url": "/diegotauchert-reactjs/static/media/mysql.d4dd0ae0.png"
+  },
+  {
+    "revision": "270f55fffea36219b66a63fbe03ff497",
+    "url": "/diegotauchert-reactjs/static/media/node.270f55ff.png"
+  },
+  {
+    "revision": "f3616b91c0ef330fc54c04a8ee04f4ac",
+    "url": "/diegotauchert-reactjs/static/media/nuvem-logo.f3616b91.svg"
+  },
+  {
+    "revision": "51417afc5342347491c41f2fa241356d",
+    "url": "/diegotauchert-reactjs/static/media/react.51417afc.png"
+  },
+  {
+    "revision": "0b0e7c19710e9695503b50927ba692dd",
+    "url": "/diegotauchert-reactjs/static/media/rocket.0b0e7c19.svg"
+  },
+  {
+    "revision": "abba4eaa03149fe7ffd8d46bbfd9e4d7",
+    "url": "/diegotauchert-reactjs/static/media/sprites.abba4eaa.png"
+  },
+  {
+    "revision": "e7f0816d35b03ce5e59979673d1436a6",
+    "url": "/diegotauchert-reactjs/static/media/tv.e7f0816d.png"
+  }
+]);
